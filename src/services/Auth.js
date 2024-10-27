0ec3a7a8fb1a5d9e4bcba518dc64f3d9cc008b82
email <marja.kokko@student.careeria.fi>
2024-10-27 21:01:22 +0200

    uusi yritys

---
+++ src/services/Auth.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseUrl = "https://localhost:7226/api/authentication"
+
+const authenticate = (userForAuth) => {
+    const request = axios.post(baseUrl, userForAuth)
+    return request.then(response => response)
+}
+
+export default { authenticate }
